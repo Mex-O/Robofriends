2d1305a1018467ebbe4ca872eb434302364bd0e2
mex-dev <aderibigbeoluwaseun4@gmail.com>
2020-08-14 18:15:28 +0100

    Updates

---
+++ precache-manifest.361fa8e5fbacc9a6c30955b087954642.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b3c368dad6cf391aeff4a20a90f5903",
+    "url": "/Robocop/index.html"
+  },
+  {
+    "revision": "4d2b39352608e64551f6",
+    "url": "/Robocop/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "464048e0a7c3e8e5ca28",
+    "url": "/Robocop/static/css/main.c6d14fd9.chunk.css"
+  },
+  {
+    "revision": "4d2b39352608e64551f6",
+    "url": "/Robocop/static/js/2.d3e727b5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Robocop/static/js/2.d3e727b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "464048e0a7c3e8e5ca28",
+    "url": "/Robocop/static/js/main.cef71d55.chunk.js"
+  },
+  {
+    "revision": "35ed3a5563cdaa1b42ac",
+    "url": "/Robocop/static/js/runtime-main.67df4d1f.js"
+  }
+]);
